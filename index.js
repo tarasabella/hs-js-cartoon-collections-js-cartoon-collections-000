79c65693e4596527188a6a69818cb2a80eb055a9
Tara E. Sabella <tarasabella@gmail.com>
2018-10-09 17:59:58 +0000

    Done.

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ for (let i = 0; i < dwarves.length; i++) {
 dwarvesList.push(`${i + 1}. ${dwarves[i]} `);
 }
 /*create a new array to hold the numbered list
-initialize i at 0, set condition at length of array, increment by 1 for each initialization
-push to the index + 1. for elements in dwarf array with each of their names */
+initialize i at 0, set condition so that i is less than length of array, increment by 1 for each initialization
+push to the index (starting at 0)+ 1. for elements in dwarf array with each of their names */
     return dwarvesList.join('');
     }
-    //join() method lets you group the elements of the dwarf array into a string. (seperator)
+    //join() method lets you group the elements of the dwarf array into a string. (seperator, comma by default)
 function summonCaptainPlanet(planeteerCalls) {
  var calls = [];
  for (let i = 0; i < planeteerCalls.length; i++) {
@@ -16,6 +16,8 @@ function summonCaptainPlanet(planeteerCalls) {
 }
 return calls;
 }
+/*create an empty array within function to store calls uppercase with !, create a for loop, and push each element of
+planeteerCalls to the end of the new array in  uppercase and adding a ! */
 
 function longPlaneteerCalls(words) {
   for (var i = 0; i < words.length; i++ ) {
@@ -25,6 +27,9 @@ function longPlaneteerCalls(words) {
     return false; }
 }
 }
+
+/* function w/ parameter words creates a for loop that returns true if each element in the words array is greater than
+4 characters and false is less than 4 */
 
 function findTheCheese (foods) {
 var cheese = ["cheddar", "gouda", "camambert"]
@@ -36,3 +41,7 @@ var cheese = ["cheddar", "gouda", "camambert"]
       }
   }
 }
+/*function accepts the parameter of an array of foods
+create a variable to hold cheese values
+for loop that will execute and return the specific cheese in the array if the argument includes that particular cheese
+otherwise it will return the string no cheese */
